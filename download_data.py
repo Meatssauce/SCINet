14caf3bcff7a5607a8cc9f09d105f8f2fccd04a9
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-09-09 14:41:14 +1000

    bug fixes

--- download_data.py
+++ download_data.py
@@ -3,4 +3,4 @@ from Historic_Crypto import Cryptocurrencies, HistoricalData, LiveCryptoData
 
 data = HistoricalData('ETH-USD', 300, '2019-09-01-00-00').retrieve_data()
 
-data.to_csv('ETH-USD-2020-06-01.csv', index=False)
+data.to_csv('ETH-USD-2020-06-01.csv')
