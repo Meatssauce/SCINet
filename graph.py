19678209934923b032b82b3dc4e52472162fdc5d
Meatssauce <70827089+Meatssauce@users.noreply.github.com>
2021-09-10 23:52:53 +1000

    bug fixes

--- graph.py
+++ graph.py
@@ -1,31 +1,28 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-from main import degree_of_differencing, look_back_window, forecast_horizon, stride, data_filepath, y_col
+from main import degree_of_differencing, look_back_window, forecast_horizon, stride, data_filepath, y_col, index_col
 from preprocessing import ARIMAPreprocessor
 
 
 # Load and preprocess data
-data = pd.read_csv(data_filepath)
-columns = data.drop(columns=['time']).columns
-time = data['time']
+data = pd.read_csv(data_filepath, index_col=index_col)
+columns = data.columns
 
 # train_data, test_data = train_test_split(data, test_size=0.2, shuffle=True, random_state=42)
 train_data = data[:int(0.6 * len(data))]
 val_data = data[int(0.6 * len(data)):int(0.8 * len(data))]
 test_data = data[int(0.8 * len(data)):]
 
-degree_of_differencing = 2
+idx = train_data.index
 preprocessor = ARIMAPreprocessor(y_col, look_back_window, forecast_horizon, stride, degree_of_differencing,
-                                 splitXy=False)
-data = preprocessor.fit_transform(train_data)
+                                 splitXy=False, relative_diff=False, scaling='standard')
+train_data = preprocessor.fit_transform(train_data)
 
 start = 0 if degree_of_differencing < 1 else 1
-
 new_columns = [col + f'{i}' for i in range(start, degree_of_differencing + 1) for col in columns]
-data = pd.DataFrame(data, columns=new_columns)
-data['time'] = pd.to_datetime(time)
-data.plot('time', 'close0')
+train_data = pd.DataFrame(train_data, columns=new_columns)
+train_data[index_col] = idx
+train_data.plot(index_col, y_col+'0')
 
 print()
